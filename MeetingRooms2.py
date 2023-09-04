5fbb59a53826b44c444bfe0d5d4e50dd4a50c88f
김상록 <sanglokkim@gimsanglog-ui-MacBookPro.local>
2023-09-04 22:51:42 +0900

    MeetingRooms2.py

---
+++ MeetingRooms2.py
@@ -0,0 +1,25 @@
+
+def minMeetingRooms(intervals):
+    intervals = sorted(intervals)
+    if len(intervals) == 1:
+        return 1
+        
+    end_time = []
+    for s,e in intervals:
+
+        if len(end_time) == 0:
+            end_time.append(e)
+            continue
+        soonest_e = end_time[0]
+        if soonest_e <= s:
+            end_time.pop(0)
+            end_time.append(e)
+            end_time = sorted(end_time)
+        elif soonest_e > s:
+            end_time.append(e)
+            end_time = sorted(end_time)
+            
+    return len(end_time)
+            
+test = [[0,30],[5,10],[15,20]]
+print(minMeetingRooms(test))
