bb29914fb8a3af804f7645347198aa5c41ce431e
김상록 <sanglokkim@gimsanglog-ui-MacBookPro.local>
2023-09-06 20:57:35 +0900

    Hashing Group Anagrams

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,23 +1,51 @@
-def groupAnagrams(strs):
-    output = []
+# def groupAnagrams(strs):
+#     output = []
 
-    while len(strs) > 0:
-        print(strs)
-        l = [strs[0]]
-        i = 1
-        pop_list = [0]
-        for i in range(1, len(strs)):
-            print(l[0], "vs", strs[i])
-            if sorted(l[0]) == sorted(strs[i]):
-                l.append(strs[i])
-                pop_list.append(i)
+#     while len(strs) > 0:
+#         print(strs)
+#         l = [strs[0]]
+#         i = 1
+#         pop_list = [0]
+#         for i in range(1, len(strs)):
+#             if sorted(l[0]) == sorted(strs[i]):
+#                 l.append(strs[i])
+#                 pop_list.append(i)
 
-        for j in range(len(pop_list)):
-            strs.pop(pop_list[j] - j)
-        print(l)
-        print(pop_list)
+#         for j in range(len(pop_list)):
+#             strs.pop(pop_list[j] - j)
+#         print(l)
+#         print(pop_list)
 
-        output.append(l)
-    return output
+#         output.append(l)
+#     return output
     
+# print(groupAnagrams(["",""]))
+
+
+def groupAnagrams(strs): ## using Hash
+    hash_dict = {}
+    for str in strs:
+        sorted_str = "".join(sorted(str))
+        if sorted_str in hash_dict:
+            hash_dict[sorted_str].append(str)
+        else:
+            hash_dict[sorted_str] = [str]
+    return list(hash_dict.values())
+
 print(groupAnagrams(["",""]))
+
+# d = {"a":["a","a"], "b":["b","b"]}
+# print(d)
+
+# d["a"].append("a")
+# print(d)
+
+# d["c"] = ["c"]
+# print(d)
+
+# if d["e"]:
+#     d["e"].append("e")
+# else:
+#     d["e"] = ["e"]
+    
+# print(d)
