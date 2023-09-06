28e8e5a1c9b963c58f8f6fdba3dc6d9a53a2a8e8
김상록 <sanglokkim@gimsanglog-ui-MacBookPro.local>
2023-09-06 19:41:22 +0900

    GroupAnagrams

---
+++ GroupAnagrams.py
@@ -0,0 +1,23 @@
+
+def groupAnagrams(strs):
+    output = []
+
+    while len(strs) > 0:
+        print(strs)
+        l = [strs[0]]
+        i = 1
+        pop_list = [0]
+        for i in range(1, len(strs[1:])):
+            if sorted(l[0]) == sorted(strs[i]):
+                l.append(strs[i])
+                pop_list.append(i)
+
+        for j in range(len(pop_list)):
+            strs.pop(pop_list[j] - j)
+        print(l)
+        print(pop_list)
+
+        output.append(l)
+    return output
+    
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
