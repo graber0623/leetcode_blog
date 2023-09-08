fa05170cf2f54bca44c7ce3a8f7d179ef87a5076
SanglokKim <graber1991@naver.com>
2023-09-08 19:18:33 +0900

    not yes

---
+++ CourseSchedule.py
@@ -0,0 +1,30 @@
+# def cs(numCourses, prerequisites): ## WRONG!!! circular it is ok until hash map
+#     d = {}
+#     for pre in prerequisites:
+#         if pre[0] in d:
+#             d[pre[0]].append(pre[1])
+#         else:
+#             d[pre[0]] = [pre[1]]
+
+#     for i, set in enumerate(d.items()):
+#         key = set[0]
+#         list_values = set[1]
+#         for value in list_values:
+#             if value in d:
+#                 if key in d[value]:
+#                     return "false"
+            
+#     return "true"
+
+# print(cs(2, [[1,0],[0,1]]))
+
+# a = {1: [0, 1], 0: [1, 1]}
+
+def cs(numCourses, prerequisites): 
+    d = {}
+    for pre in prerequisites:
+        if pre[0] in d:
+            d[pre[0]].append(pre[1])
+        else:
+            d[pre[0]] = [pre[1]]
+
