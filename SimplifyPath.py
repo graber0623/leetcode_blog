b780e1422a3b3b2e0a328f43a9bddc59e4ab3ccb
SanglokKim <graber1991@naver.com>
2023-09-12 12:56:32 +0900

    '..' wont be removed...

---
+++ SimplifyPath.py
@@ -0,0 +1,23 @@
+path1 = "/home/"
+path2 = "/../"
+path3 = "/home//foo/"
+
+# print(path1.split("/"))
+# print(path2.split("/"))
+# print(path3.split("/"))
+
+def simplifyPath(path):
+    path_list = path.split("/")
+    print(path_list)
+    remover = ["",".",".."]
+    for dirname in path_list:
+        if dirname in remover:
+            path_list.remove(dirname)
+    print(path_list)
+    res = "/" + "/".join(path_list)
+    print("========")
+    return res
+
+print(simplifyPath(path1))
+print(simplifyPath(path2))
+print(simplifyPath(path3))
