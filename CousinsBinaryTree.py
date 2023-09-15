ca3b3952c3bd5fae63cc6a6fa05d159df5da15cf
SanglokKim <graber1991@naver.com>
2023-09-15 21:14:43 +0900

    it works but....

---
+++ CousinsBinaryTree.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+d = defaultdict(list)
+
+# d[1] = [11]
+# d[2] = [22]
+# d[3] = [33]
+
+# print(list(d.keys()))
+
+# print(d[1])
+if not d:
+    print(True)
+else:
+    print(False)
