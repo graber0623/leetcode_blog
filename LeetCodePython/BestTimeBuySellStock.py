96910190b8163822df3ac062f06fa79e90224315
SanglokKim <graber1991@naver.com>
2023-09-21 13:19:24 +0900

    i didnt understand bestbuytimeandstock

---
+++ LeetCodePython/BestTimeBuySellStock.py
@@ -0,0 +1,23 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int], fee: int) -> int:
+        
+def maxProfit(prices, fee) -> int:
+    print(prices)
+    pos=-prices[0]
+    print(pos)
+    profit=0
+    n=len(prices)
+    for i in range(1,n):
+        print("pos : ",pos, "   ", profit-prices[i])
+        pos=max(pos,profit-prices[i])
+        
+        print("profit : " ,profit, "    ", pos+prices[i]-fee)
+        profit=max(profit,pos+prices[i]-fee)
+        
+        print("changed : ",pos, "    ", profit)
+
+
+    return profit 
+
+print(maxProfit([10,2,9,3,1,4,9], 2))
+
