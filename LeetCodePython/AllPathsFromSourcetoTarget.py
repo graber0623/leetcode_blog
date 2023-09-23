25030a4aacc9283ddbd01bb2d547c50535cb8a9a
SanglokKim <graber1991@naver.com>
2023-09-23 17:56:10 +0900

    much more easier answer and to understand

--- LeetCodePython/AllPathsFromSourcetoTarget.py
+++ LeetCodePython/AllPathsFromSourcetoTarget.py
@@ -49,3 +49,29 @@ class Solution:
 
         dfs(graph[0], [], 0)
         return paths
+    
+    
+    
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        # Initialize an empty list to store the paths
+        ans = []
+        
+        # Define a recursive function to perform DFS
+        def dfs(node, path):
+            # If the current node is the target node, add the path to the answer list
+            if node == len(graph) - 1:
+                ans.append(path)
+                return
+            # Iterate through the neighbors of the current node
+            for n in graph[node]:
+                # Recursively call the function on the neighbor, passing in a copy of the path list with the neighbor appended
+                dfs(n, path + [n])
+        
+        # Start the DFS search from the source node (node 0)
+        dfs(0, [0])
+        
+        # Return the list of paths
+        return ans
+
+
