3402431732a1334cba985d962325a5f469205c4e
SanglokKim <graber1991@naver.com>
2023-10-06 13:49:51 +0900

    movingaverage

---
+++ LeetCodePython/MovingAveragefromDataStream.py
@@ -0,0 +1,23 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = collections.deque()
+        self.sum = 0
+        
+
+    def next(self, val: int) -> float:
+        self.queue.append(val)
+        self.sum += val
+        
+        if len(self.queue) >  self.size:
+            self.sum  -= self.queue.popleft()
+        
+        return self.sum/len(self.queue)
+            
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
