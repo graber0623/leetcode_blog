17464c4cc1f7a438ee4077cc221a31aecded50e4
SanglokKim <graber1991@naver.com>
2023-10-09 18:48:42 +0900

    Not My solution

--- LeetCodePython/MyHashMap.py
+++ LeetCodePython/MyHashMap.py
@@ -1,25 +1,62 @@
+# class MyHashMap: 
+
+#     def __init__(self):
+#         self.map = []
+
+#     def put(self, key: int, value: int) -> None:
+#         for attribute in self.map:
+#             if key == attribute[0]:
+#                 self.map.remove(attribute)
+#         self.map.append([key, value])
+#         return
+
+#     def get(self, key: int) -> int:
+#         for attribute in self.map:
+#             if key == attribute[0]:
+#                 return attribute[1]
+#         return -1
+
+#     def remove(self, key: int) -> None:
+#         for attribute in self.map:
+#             if key == attribute[0]:
+#                 self.map.remove(attribute)
 class MyHashMap:
-
     def __init__(self):
-        self.map = []
+        self.size = 10
+        self.mp = [[] for _ in range(self.size)]
 
-    def put(self, key: int, value: int) -> None:
-        for attribute in self.map:
-            if key == attribute[0]:
-                self.map.remove(attribute)
-        self.map.append([key, value])
-        return
+    def hash(self, key):
+        return key % self.size
 
-    def get(self, key: int) -> int:
-        for attribute in self.map:
-            if key == attribute[0]:
-                return attribute[1]
+    def put(self, key, value):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for idx, pair in enumerate(bucket):
+            if pair[0] == key:
+                bucket[idx] = (key, value)
+                return
+
+        bucket.append((key, value))
+
+    def get(self, key):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for pair in bucket:
+            if pair[0] == key:
+                return pair[1]
+
         return -1
 
-    def remove(self, key: int) -> None:
-        for attribute in self.map:
-            if key == attribute[0]:
-                self.map.remove(attribute)
+    def remove(self, key):
+        i = self.hash(key)
+        bucket = self.mp[i]
+
+        for idx, pair in enumerate(bucket):
+            if pair[0] == key:
+                del bucket[idx]
+                return
         
         
         
