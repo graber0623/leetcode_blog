9a15ab6f5e9c3da0ad3453fe562b26759c68d661
SanglokKim <graber1991@naver.com>
2023-10-11 20:40:46 +0900

    slow

---
+++ LeetCodePython/MinimumNumberOfOpMoveAllBallks.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        ans = [0] * n
+
+        for i in range(n):
+            for j in range(n):
+                if boxes[j] == "1" and i != j:
+                    ans[i] += abs(i - j)
+        
+        return ans
