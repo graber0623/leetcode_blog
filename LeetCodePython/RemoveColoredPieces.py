ef39201d07ce60af8746c04e342ff365e8541572
SanglokKim <graber1991@naver.com>
2023-10-22 15:57:58 +0900

    Read The Question

---
+++ LeetCodePython/RemoveColoredPieces.py
@@ -0,0 +1,21 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        cl= list(colors)
+        i = 1
+        remove_count = 0
+
+        while i < len(cl)-1:
+
+            if cl[i-1] == cl[i] and cl[i] == cl[i+1]:
+                remove_count +=1
+                cl.pop(i)
+            
+            else:
+                i+=1
+
+        if remove_count % 2 == 1:
+            return True
+        else:
+            return False
+
+
