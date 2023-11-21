f704e844c0a6484a54a43afe1c324ce2141a8232
SanglokKim <graber1991@naver.com>
2023-11-21 19:41:01 +0900

    timelimit

---
+++ LeetCodePython/3SumClosest.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        a = combinations(nums, 3)
+        ans = 1000000
+        for sub in a:
+            if abs(sum(sub) - target) < abs(ans - target):
+                ans = sum(sub)
+        return ans
