860d2675cbd33c8bd266bdc44198d01a5b1d001d
SanglokKim <graber1991@naver.com>
2023-11-23 20:35:36 +0900

    med

---
+++ LeetCodePython/EqualtoAverageSubtree.py
@@ -0,0 +1,58 @@
+import math
+from collections import defaultdict
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+        d = defaultdict(list)
+        def appender(node, keys):
+
+            if not node:
+                return 
+
+            new_key = str(node.val) + 
+            keys.append(node.val)
+
+            for key in keys:
+                d[key].append(node.val)
+
+            appender(node.left, keys.copy())
+            appender(node.right, keys.copy())
+
+        appender(root, [])
+        c = 0
+        for k in d.keys():
+            if k == math.floor(mean(d[k])):
+
+                c+=1
+
+        
+        return c
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+        ans=0
+        def traverse(node):
+            if node is None:
+                return (0,0)
+            left=traverse(node.left)
+            right=traverse(node.right)
+            total=left[0]+right[0]+node.val
+            count=left[1]+right[1]+1
+            if total//count==node.val:
+                nonlocal ans
+                ans+=1
+            return (total,count)
+        traverse(root)
+        return ans       
