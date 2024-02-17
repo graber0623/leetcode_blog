bba6566f8751242bae176fd61c3fca44cf7d33cd
SanglokKim <graber1991@naver.com>
2024-02-17 15:05:15 +0900

    med

---
+++ LeetCodePython/jumpgamedfs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        ans = False
+        n = len(nums)
+        def dfs(i):
+            nonlocal nums, n, ans
+
+            if i >= n-1:
+                ans = True
+                return
+
+            if nums[i] == 0:
+                return
+                
+            for j in range(1, nums[i]+1):
+                dfs(i+j)
+            
+        dfs(0)
+        return ans
