e2ae93de910d75403481965e9025a2ee40b4b85a
SanglokKim <graber1991@naver.com>
2024-02-23 20:43:40 +0900

    med

---
+++ LeetCodeSQL/AllCommonPair.sql
@@ -0,0 +1,29 @@
+-- # Write your MySQL query statement below
+-- with cte as(
+-- select
+-- r1.user_id as user1_id , r2.user_id as user2_id, count(*) as cn
+-- from Relations R1
+-- inner join Relations R2
+-- on R1.user_id < R2.user_id and R1.follower_id = R2.follower_id
+-- group by r1.user_id, r2.user_id
+-- )
+
+-- select
+--  user1_id, user2_id
+-- from cte where cn = (select max(cn) from cte)
+
+-- -- group by R1.user_id, R2.user_id
+
+with cte as (
+select
+R1.user_id as user1_id, R2.user_id as user2_id, count(*) as c
+from Relations R1
+inner join Relations R2
+on R1.user_id < R2.user_id and R1.follower_id = R2.follower_id
+group by R1.user_id, R2.user_id
+)
+
+select
+user1_id, user2_id
+from cte 
+where c = (select max(c) from cte)
