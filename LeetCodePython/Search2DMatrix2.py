a6e72e4e7d8fa166a398f9d572777f02548b9f68
SanglokKim <graber1991@naver.com>
2024-04-22 19:12:37 +0900

    med

---
+++ LeetCodePython/Search2DMatrix2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix[0])
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == target:
+                    return True
+                
+                if matrix[i][j] > target:
+                    break
+
+        
+        return False
