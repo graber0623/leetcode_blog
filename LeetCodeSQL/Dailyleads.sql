ffa0dad4edf28b3b8d2867b9591abf1ffca38f41
SanglokKim <graber1991@naver.com>
2024-05-12 16:26:40 +0900

    med

--- LeetCodeSQL/Dailyleads.sql
+++ LeetCodeSQL/Dailyleads.sql
@@ -7,3 +7,11 @@ SELECT date_id, make_name, COUNT(DISTINCT(lead_id)) AS unique_leads, COUNT(DISTI
 FROM DailySales
 
 GROUP BY date_id, make_name
+
+/* Write your T-SQL query statement below */
+SELECT
+T1.date_id, T1.make_name,
+count(DISTINCT T1.lead_id) as unique_leads,
+count(DISTINCT T1.partner_id) as unique_partners
+FROM DailySales T1
+GROUP BY T1.date_id, T1.make_name
