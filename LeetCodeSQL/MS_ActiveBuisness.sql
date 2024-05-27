a10a1518d06b8f7d536d969c7e6b73788b4047d2
SanglokKim <graber1991@naver.com>
2024-05-27 19:42:59 +0900

    med

--- LeetCodeSQL/MS_ActiveBuisness.sql
+++ LeetCodeSQL/MS_ActiveBuisness.sql
@@ -7,6 +7,19 @@ with cte as (
 
 select
 distinct business_id
+from Events T1/* Write your T-SQL query statement below */
+with cte as (
+    select event_type, AVG(occurrences) as avg_ccc
+    from Events
+    group by event_type
+)
+
+select
+business_id
 from Events T1
+left join cte T2 on T1.event_type = T2.event_type
+where occurrences > T2.avg_ccc
+group by business_id
+having count(T2.event_type) > 1
 left join cte T2 on T1.event_type = T2.event_type
 where occurrences >= T2.avg_ccc
