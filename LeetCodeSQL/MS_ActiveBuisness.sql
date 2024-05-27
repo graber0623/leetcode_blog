aada859d9817a29289c18cd149ca4243ab4c0eaa
SanglokKim <graber1991@naver.com>
2024-05-27 19:39:20 +0900

    med

---
+++ LeetCodeSQL/MS_ActiveBuisness.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+with cte as (
+    select event_type, AVG(occurrences) as avg_ccc
+    from Events
+    group by event_type
+)
+
+select
+distinct business_id
+from Events T1
+left join cte T2 on T1.event_type = T2.event_type
+where occurrences >= T2.avg_ccc
